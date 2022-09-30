adb053c9bbe9c33b81548f396172362046ccdd77
Amelie <103225509+dunn-am25@users.noreply.github.com>
2022-09-30 19:46:23 +1300

    Update and rename canteen_page_1.html to canteen_home.php

--- canteen_home.php
+++ canteen_home.php
@@ -1,40 +1,53 @@
-    <!DOCTYPE html>
+       <!DOCTYPE html>
     <html lang="en">
         <head>
+            <!-- Heading, beginning information, link to stylesheet -->
             <meta charset="UTF-8">
             <title>Wellington Girls' College Canteen</title>
             <meta name="description" content="WGC Home Page Layout">
             <link href="canteen_style.css" rel="stylesheet" type="text/css">
         </head>
         <body>
+
+            <!-- The grid layout begins within a container. -->
+
              <div class="grid-container">
                  <div class="grid-item logo">
 
                  </div>
                  <div class="grid-item header">
-                     <img src="https://wgc.school.nz/wp-content/uploads/2018/09/WGC_Logo_Transparent_RGB.png"
-                          width="53%" height="70%"
-                     alt="The logo of the school."> <p> <b> WGC Canteen </b> </p>
+                     <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_home.php">
+                         <img src="https://wgc.school.nz/wp-content/uploads/2018/09/WGC_Logo_Transparent_RGB.png"
+                              width="53%" height="70%"
+                              alt="The logo of the school.">
+                     </a>
+                     <p> <b> WGC Canteen </b> </p>
                  </div>
                  <div class="grid-item navigation-1">
-                     <p> <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_page_1.html">HOME</a>
+                     <p> <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_home.php">HOME</a>
                      </p>
                  </div>
                  <div class="grid-item navigation-2">
-                     <p> <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_page_2.html">FOOD MENU</a>
+                     <p> <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_food.php">FOOD MENU</a>
                      </p>
                  </div>
                  <div class="grid-item navigation-3">
-                     <p> <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_page_3.html">DRINK MENU</a>
+                     <p> <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_drinks.php">DRINK MENU</a>
                      </p>
                  </div>
+
+                 <!-- Each of the specials is shown for each day of the week. -->
+
                  <div class="grid-item special-display">
                      <p> <b> Welcome to the official website for the Wellington Girls' College Canteen. </b> <br>
-                     <br> Monday Special: Cheese Scone
-                     <br> Tuesday Special: Ham Panini
-                     <br> Wednesday Special: Mini Cheese Pizza
-                     <br> Thursday Special: Choc Cupcake
-                     <br> Friday Special: Hot Chips <br>
+                     <br> Monday Special: Ham Panini, Cola
+                     <br> Tuesday Special: Cheese Scone, Orange Juice
+                     <br> Wednesday Special: Mini Cheese Pizza, Chocolate Milk
+                     <br> Thursday Special: Choc Cupcake, Water
+                     <br> Friday Special: Hot Chips, Chai Tea <br>
+
+                         <!-- A link is provided to the other two pages of the website. -->
+
                      <br> <b> CHECK THE DETAILS OF OUR SPECIALS FOR
                              <a href="http://dtweb.wgc.school.nz/dunnam/school_canteen/canteen_page_2.html">FOOD</a>
                              AND FOR
@@ -42,6 +55,9 @@
                      </b> </p>
 
                  </div>
+
+                 <!-- Other general information about the WGC Canteen is displayed. -->
+
                  <div class="grid-item openhours-display">
                      <p>WHEN ARE WE OPEN?
                      <br> We are open on school days from 8:45am - 3:20pm. </p>
